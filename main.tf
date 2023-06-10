8e3efa48510a2bd89e6be3cb2a729c465370b1ab
aniket-sinha8 <sinhaaniket32@gmail.com>
2023-06-11 01:15:52 +0530

    changed ubuntu sku

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "azurerm_virtual_machine" "vm" {
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
   storage_os_disk {
