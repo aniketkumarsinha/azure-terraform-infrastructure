dc2938372af8e81ab91f4327616f390eca4f44a9
aniket-sinha8 <sinhaaniket32@gmail.com>
2023-06-10 23:49:33 +0530

    corrected code

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 # Configure Azure Provider
 terraform {
   required_providers {
-    azurerm = "hashicorp/azurerm"
-    version = ">= 2.26"
+    azurerm {
+      source = "hashicorp/azurerm"
+      version = ">= 3.59.0"
+    } 
   }
   required_version = ">= 0.14.9"
 }
