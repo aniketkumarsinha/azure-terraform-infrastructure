22173dca1148b2a6bca04b85e1a088a3423ccba7
aniket-sinha8 <sinhaaniket32@gmail.com>
2023-06-11 00:25:28 +0530

    added ids to connect with Azure

--- main.tf
+++ main.tf
@@ -15,10 +15,10 @@ provider "azurerm" {
   skip_provider_registration = "true"
   
   # Connection to Azure
-  # subscription_id = var.subscription_id
-  # client_id = var.client_id
-  # client_secret = var.client_secret
-  # tenant_id = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
 }
 
 variable "prefix" {
