e77bc21a2d620d6a1b9fe010711faf0634d0427b
Aniket Kumar Sinha <56578304+aniketkumarsinha@users.noreply.github.com>
2024-02-10 21:14:03 +0530

    updated vm size

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "azurerm_virtual_machine" "vm" {
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.nic.id]
-  vm_size               = "Standard_D1_v2"
+  vm_size               = "Standard_DS1_v2"
 
   storage_image_reference {
     publisher = "Canonical"
